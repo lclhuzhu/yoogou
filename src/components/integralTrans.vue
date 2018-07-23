9f97de59251887226e9813cb9ff0a759547ebff0
480858838@qq.com <mac@macdeiMac.local>
2018-07-23 15:44:45 +0800

    买入页面

--- src/components/integralTrans.vue
+++ src/components/integralTrans.vue
@@ -1,14 +1,13 @@
 <template>
 	<div class="integralTrans">
 		<van-nav-bar title="买入" left-text="返回" left-arrow @click-left="onClickLeft"/>
-	    <van-cell-group class="reset">
+	    <van-cell-group v-for="item in 5">
 	    	<router-link to="/buyinDetail">
-	    		<van-cell class="cell" title="产品名称产品名称" value="2,000" label="2018-01-15 10:15" ></van-cell>
+	    		<van-cell class="flex_center" title="产品名称产品名称" value="2000" label="2018-01-15 10:15" ></van-cell>
 	    	</router-link>
-	      
 	    </van-cell-group>
 	</div>
-	
+
   <!--<van-popup v-model="isShow">-->
     <!--<p class="pop1">提示</p><img src="">-->
     <!--<p class="pop2">您目前无法购买该产品</p>-->
@@ -31,50 +30,16 @@
 		    onClickLeft() {
 		      	history.go(-1)
 		    },
-		    
+
 	  	}
     }
 </script>
 
 <style scoped>
-/*.reset .van-cell__title span {*/
-    /*font-size: .28rem;*/
-    /*color: #222;*/
-    /*margin-left: .3rem;*/
-    /*margin-top: .32rem;*/
-  /*}*/
-/*.van-cell__title div {*/
-      /*font-size: .24rem;*/
-      /*color: red;*/
-      /*margin-left: .3rem;*/
-  /*}*/
-.van-cell__value span {
-  color: deeppink;
-
-}
 </style>
 <style>
-  .reset {
-    padding-top: .32rem;
-    height: 1.4rem;
-  }
-  .reset .van-cell__title span {
-    font-size: .28rem;
-    color: #222;
-    margin-left: .3rem;
-  }
-  .reset .van-cell__title div {
-    font-size: .24rem;
-    color: #999999;
-    margin-left: .3rem;
-    margin-top: .1rem;
-  }
-  .reset .van-cell__value {
-    padding-top: .24rem;
-  }
-  .van-cell__value span {
-    color: #FF6A08;
-    font-size: .36rem;
-  }
-  .pop1 {}
+  .van-cell {height: 1.4rem}
+  .van-cell__title span { font-size: .28rem; color: #222; }
+  .van-cell__value span { color: #FF6A08; font-size: .36rem; }
+  .van-cell__label {font-size: .24rem; margin-top: 0.08rem; }
 </style>
